266a3235106300a2e95fb9aca081ff558a3a28ba
Danny Marsland <danny.marsland@gmail.com>
2015-04-18 16:31:36 +0100

    further commenting for typescript files

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -3,31 +3,39 @@ var Application = (function () {
     function Application() {
         var _this = this;
         this.resultsSelector = '#results';
+        // call the class ready function on ready
         $(document).ready(function () { return _this.ready(); });
     }
     Application.prototype.ready = function () {
+        //create lightBox and form objects, keep local copy in case we need to interact with them later
         var _this = this;
         this.lightBox = new LightBox();
         // enhance the form to submit via ajax, and then show the results in a light box
-        this.form = new ValueForm($('#value-form'), function () { return _this.showLoading(); }, function (data) {
+        this.form = new ValueForm($('#value-form'), 
+        // show loading on submit
+        function () { return _this.showLoading(); }, function (data) {
+            // hide loading and process the returned data
             _this.hideLoading();
             var $results = _this.parseResults(data);
+            // show the results
             _this.lightBox.show($results);
         }, function () {
+            // hide loading and inform user of an error
             _this.hideLoading();
             _this.showError('An network error has occurred, please try again');
         });
-        // if we have gone directly to a page with results on it then show the results in the lightbox
+        // if we have gone directly to a page with results on it then show the results in the lightBox
         var $results = $(this.resultsSelector);
         if ($results.length) {
             // remove from dom as featherlight appears to copy the content and not move it
-            // on a timeout as featherlight doesnt animate well without
+            // on a timeout as featherlight doesn't animate well without
             setTimeout(function () {
                 _this.lightBox.show($results);
             }, 200);
         }
     };
     Application.prototype.parseResults = function (html) {
+        // extract the results from the html string
         return $(html).find(this.resultsSelector).first();
     };
     Application.prototype.showLoading = function () {
@@ -49,17 +57,21 @@ var ValueForm = (function () {
         this.onSubmit = onSubmit;
         this.onSuccess = onSuccess;
         this.onError = onError;
+        // submit via ajax
         $form.submit(function (ev) {
+            // call the onSubmit callback
             _this.onSubmit();
             $.ajax({
                 type: $form.attr('method'),
                 url: $form.attr('action'),
                 data: $form.serialize(),
                 success: function (data) {
+                    // call the onSuccess callback
                     _this.onSuccess(data);
                 },
                 error: function (data) {
                     console.error('Error submitting form');
+                    // call the onError callback
                     _this.onError();
                 }
             });
--- public/static/js/app.ts
+++ public/static/js/app.ts
@@ -1,6 +1,6 @@
 ///<reference path="../def/jquery/jquery.d.ts"/>
 
-// basic definition for featherlight lightbox
+// basic definition for featherlight lightBox
 interface featherlight {
     (content: any, config? : any) : JQueryStatic
 }
@@ -16,29 +16,36 @@ class Application {
     private form: ValueForm;
 
     constructor() {
+        // call the class ready function on ready
         $(document).ready(()=>this.ready());
     }
 
     private ready() {
+        //create lightBox and form objects, keep local copy in case we need to interact with them later
+
         this.lightBox = new LightBox();
         // enhance the form to submit via ajax, and then show the results in a light box
         this.form = new ValueForm($('#value-form'),
+            // show loading on submit
             ()=>this.showLoading(),
             (data) => {
+                // hide loading and process the returned data
                 this.hideLoading();
                 var $results = this.parseResults(data);
+                // show the results
                 this.lightBox.show($results);
             }, () => {
+                // hide loading and inform user of an error
                 this.hideLoading();
                 this.showError('An network error has occurred, please try again')
             }
         );
 
-        // if we have gone directly to a page with results on it then show the results in the lightbox
+        // if we have gone directly to a page with results on it then show the results in the lightBox
         var $results = $(this.resultsSelector);
         if ($results.length) {
             // remove from dom as featherlight appears to copy the content and not move it
-            // on a timeout as featherlight doesnt animate well without
+            // on a timeout as featherlight doesn't animate well without
             setTimeout(()=>{
                 this.lightBox.show($results)
             },200);
@@ -47,6 +54,7 @@ class Application {
     }
 
     private parseResults(html: string) : JQuery {
+        // extract the results from the html string
         return $(html).find(this.resultsSelector).first();
     }
 
@@ -67,17 +75,21 @@ class Application {
 
 class ValueForm {
     constructor($form: JQuery, private onSubmit: ()=>void, private onSuccess: (data)=>void, private onError: ()=>void) {
+        // submit via ajax
         $form.submit((ev) =>{
+            // call the onSubmit callback
             this.onSubmit();
             $.ajax({
                 type: $form.attr('method'),
                 url: $form.attr('action'),
                 data: $form.serialize(),
                 success: (data) => {
+                    // call the onSuccess callback
                     this.onSuccess(data);
                 },
                 error: (data) => {
                     console.error('Error submitting form');
+                    // call the onError callback
                     this.onError();
                 }
             });
